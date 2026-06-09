fb7e4c59deb27faff79d84fdfac1049c387d7383
FRANCEN SELLOTE <159442396+fsellote@users.noreply.github.com>
2026-06-09 17:22:12 +0800

    location

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,8 +8,8 @@ const person: Person = {
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "Cagayan de Oro, Philippines", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Filipino"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
